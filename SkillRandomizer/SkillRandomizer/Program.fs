a7b64f8879d3ccc6188b23ec46a6d2e52da7fdc0
Tim Ramandt <tim.ramandt@hotmail.com>
2018-07-22 01:54:06 +0200

    F# try-outs with data classes

---
+++ SkillRandomizer/SkillRandomizer/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+// Learn more about F# at http://fsharp.org
+
+//F# try-outs, references to later course
+//open System
+
+//let Greeting greeting name = greeting + " from " + name
+
+//[<EntryPoint>]
+//let main argv =
+//    Greeting "Hello" "Tim" |> printfn "%s"
+
+//    Console.ReadKey()
+//    0 // return an integer exit code
+
+[<EntryPoint>]
+printf "helleu!"
+Console.ReadKey()
+0
