89f125224ef44c249ad0c7ae4a5055ebdd67a44b
Tim Ramandt <tim.ramandt@hotmail.com>
2018-07-22 02:46:33 +0200

    WebRequest F#

--- SkillRandomizer/Program.fs
+++ SkillRandomizer/Program.fs
@@ -1,4 +1,6 @@
 ﻿open System
+open System.Net
+open System.IO
 
 // Learn more about F# at http://fsharp.org
 
@@ -14,8 +16,20 @@
 //    Console.ReadKey()
 //    0 // return an integer exit code
 
+let FetchData name = 
+    let url = "http://services.runescape.com/m=hiscore/index_lite.ws?player=" + name
+    let request = HttpWebRequest.Create(url)
+    let response = request.GetResponse() 
+    let stream = response.GetResponseStream()
+    let reader = new StreamReader(stream) 
+       
+    reader.ReadToEnd()
+ 
 [<EntryPoint>]
 let main argv = 
     printfn "Bored on RuneScape? This commandline app will determine what skill you should train next."
+    printfn "Let's start. Enter your Runescape Name"
+    let name = Console.ReadLine()
+    FetchData name |> printfn "%s"
     Console.ReadKey()
     0
